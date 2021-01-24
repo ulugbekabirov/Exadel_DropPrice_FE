a7cb4efc6ead3750b78bb7d84f2758d69c8e24b3
Denis Silkov <dzsilkov@gmail.com>
2021-01-24 03:04:43 +0300

    add shared and auth modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+
+import { SharedModule } from './shared/shared.module';
 import { AuthInterceptor } from './auth/auth.interceptor';
 import { AppComponent } from './app.component';
 import { AuthService } from './auth/auth.service';
@@ -14,7 +14,7 @@ import { AuthService } from './auth/auth.service';
   imports: [
     BrowserModule,
     HttpClientModule,
-    CommonModule,
+    SharedModule,
   ],
   providers: [
     AuthService,
