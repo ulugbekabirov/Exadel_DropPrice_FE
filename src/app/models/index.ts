5735f8f3a5815421f0a6fe2dd92b616e56da6da0
Denis Silkov <dzsilkov@gmail.com>
2021-01-30 04:01:57 +0300

    add models Tag, Town

--- src/app/models/index.ts
+++ src/app/models/index.ts
@@ -3,3 +3,6 @@ export * from './auth-info';
 export * from './discount';
 export * from './vendor';
 export * from './active-user';
+export * from './ticket';
+export * from './town';
+export * from './tag';
