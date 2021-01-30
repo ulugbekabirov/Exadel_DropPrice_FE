26451e9eea283b67b68f688543c1cb682149fb5f
Denis Silkov <dzsilkov@gmail.com>
2021-01-30 04:52:45 +0300

    add forkjoin for requests for home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { DiscountsService } from '../../services/discounts.service';
 import { UserService } from '../../services/user.service';
-import { Observable, Subscription } from 'rxjs';
+import { forkJoin, Observable, Subscription } from 'rxjs';
 import { ActiveUser } from '../../models';
+import { ApiDataService } from '../../services/api-data.service';
 import { tap } from 'rxjs/operators';
 
 
@@ -19,12 +20,25 @@ export class HomeComponent implements OnInit, OnDestroy {
   constructor(
     private discountsService: DiscountsService,
     private userService: UserService,
-  ) {}
+    private restApi: ApiDataService,
+  ) {
+  }
 
   ngOnInit(): void {
     this.activeUser$ = this.userService.activeUser;
     this.subscription = this.activeUser$
       .subscribe(user => this.activeUser = user);
+    forkJoin(
+      this.restApi.getTowns(),
+      this.restApi.getTags(0, 10),
+      this.discountsService.getDiscounts(0, 10, this.activeUser.longitude, this.activeUser.latitude, 'dist'),
+    ).pipe(
+      tap(([res1, res2, res3]) => {
+        console.log('res1', res1);
+        console.log('res2', res2);
+        console.log('res3', res3);
+      })
+    ).subscribe(x => console.log('x', x));
   }
 
   ngOnDestroy(): void {
