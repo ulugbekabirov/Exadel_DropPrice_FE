db73dd72a621b1f9640a53e5f9bf1a609ea4d8ee
Denis Silkov <dzsilkov@gmail.com>
2021-02-06 10:04:23 +0300

    add discount.module

---
+++ src/app/discounts/discounts.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SharedModule } from '../shared/shared.module';
+import { DiscountsListComponent } from '../shared/components/discounts-list/discounts-list.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: DiscountsListComponent,
+    // children: [
+    //   { path: '', component: DiscountsHomeComponent },
+    //   { path: 'new', component: DiscountEditComponent },
+    //   {
+    //     path: ':id',
+    //     component: DiscountDetailComponent,
+    //     resolve: [DiscountsResolverService]
+    //   },
+    //   {
+    //     path: ':id/edit',
+    //     component: DiscountEditComponent,
+    //     resolve: [DiscountsResolverService]
+    //   }
+    // ]
+  }
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forChild(routes),
+    SharedModule
+  ]
+})
+export class DiscountsModule { }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,12 +3,16 @@ import { SharedModule } from '../shared/shared.module';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from '../components/home/home.component';
 import { AuthGuard } from '../guards/auth.guard';
+import { DiscountsModule } from '../discounts/discounts.module';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
     canActivate: [AuthGuard],
+    children: [
+      {path: 'discounts', loadChildren: () => import ('../discounts/discounts.module.js').then(m => m.DiscountsModule)},
+    ]
   }
 ];
 
@@ -21,4 +25,5 @@ const routes: Routes = [
     RouterModule.forChild(routes),
   ]
 })
-export class HomeModule { }
+export class HomeModule {
+}
