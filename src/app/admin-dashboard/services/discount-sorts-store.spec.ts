8b182188fa22ec77f2cc7aebdb916172dd327d21
Denis Silkov <dzsilkov@gmail.com>
2021-03-03 12:41:46 +0300

    change locale, comment classes in spec.ts

--- src/app/admin-dashboard/services/discount-sorts-store.spec.ts
+++
@@ -1,7 +0,0 @@
-import { DiscountsSortStore } from './discounts-sorts-store';
-
-describe('DiscountSortsStore', () => {
-  it('should create an instance', () => {
-    expect(new DiscountsSortStore()).toBeTruthy();
-  });
-});
