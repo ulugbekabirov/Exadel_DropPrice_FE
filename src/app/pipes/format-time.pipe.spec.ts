421eb47d0e3f238cbb55b70429985f98778576f0
Anna Balkovskaya <anna.balkovskaya@gmail.com>
2021-03-06 23:59:14 +0300

    Test format-time pipe

--- src/app/pipes/format-time.pipe.spec.ts
+++ src/app/pipes/format-time.pipe.spec.ts
@@ -1,8 +1,39 @@
 import { FormatTimePipe } from './format-time.pipe';
 
 describe('FormatTimePipe', () => {
-  it('create an instance', () => {
+
+  it('should format time (sec)', () => {
+    const value = 30;
     const pipe = new FormatTimePipe();
-    expect(pipe).toBeTruthy();
+    const result = pipe.transform(value);
+    expect(result).toBe('00:00:30');
+  });
+
+  it('should format time (min)', () => {
+    const value = 180;
+    const pipe = new FormatTimePipe();
+    const result = pipe.transform(value);
+    expect(result).toBe('00:03:00');
+  });
+
+  it('should format time (hours)', () => {
+    const value = 3600;
+    const pipe = new FormatTimePipe();
+    const result = pipe.transform(value);
+    expect(result).toBe('01:00:00');
+  });
+
+  it('should format time (hours + min)', () => {
+    const value = 4800;
+    const pipe = new FormatTimePipe();
+    const result = pipe.transform(value);
+    expect(result).toBe('01:20:00');
+  });
+
+  it('should format time (hours + min + sec)', () => {
+    const value = 4810;
+    const pipe = new FormatTimePipe();
+    const result = pipe.transform(value);
+    expect(result).toBe('01:20:10');
   });
 });
