3981cdc6f1a5166bd852dac7abf353d0f8d5810c
Anna Balkovskaya <anna.balkovskaya@gmail.com>
2021-03-09 16:49:03 +0300

    Fix tslint

--- src/app/pipes/truncate-string.pipe.spec.ts
+++ src/app/pipes/truncate-string.pipe.spec.ts
@@ -3,6 +3,6 @@ import { TruncateStringPipe } from './truncate-string.pipe';
 describe('TruncateStringPipe', () => {
   it('transform "abc" to "abc...', () => {
     const pipe = new TruncateStringPipe();
-    expect(pipe.transform("Холодильник Bosch Serie 8 VitaFresh Plus KGN39LB32R", 50)).toBe("Холодильник Bosch Serie 8 VitaFresh Plus...");
+    expect(pipe.transform('Холодильник Bosch Serie 8 VitaFresh Plus KGN39LB32R', 50)).toBe('Холодильник Bosch Serie 8 VitaFresh Plus...');
   });
 });
