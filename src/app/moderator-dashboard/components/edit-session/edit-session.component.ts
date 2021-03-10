cccd62e9fb7325c6c2aee0daa21e538e52cdf824
Denis Silkov <dzsilkov@gmail.com>
2021-03-10 12:51:51 +0300

    add field discountAvailable to user orders

--- src/app/moderator-dashboard/components/edit-session/edit-session.component.ts
+++ src/app/moderator-dashboard/components/edit-session/edit-session.component.ts
@@ -1,13 +1,25 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-edit-session',
   templateUrl: './edit-session.component.html',
   styleUrls: ['./edit-session.component.scss']
 })
-export class EditSessionComponent {
-@Input() editSession$;
-@Output() onResumeEditSession: EventEmitter<any> = new EventEmitter();
+export class EditSessionComponent implements OnInit {
+  @Input() editSession$;
+  @Output() onResumeEditSession: EventEmitter<any> = new EventEmitter();
+  editSession;
+  editMessage;
+  timeEditing;
+
+  ngOnInit(): void {
+    this.editSession$.pipe()
+      .subscribe(session => {
+        console.log(session);
+        this.editSession = session;
+      });
+  }
+
   resumeEditSession($event): void {
     this.onResumeEditSession.emit($event);
   }
--- src/app/shared/components/countdown-timer/countdown-timer.component.ts
+++ src/app/shared/components/countdown-timer/countdown-timer.component.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectionStrategy, Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
-import { interval } from 'rxjs';
+import { BehaviorSubject, interval } from 'rxjs';
 import { finalize, mapTo, scan, switchMap, takeWhile, tap } from 'rxjs/operators';
 
 interface EditSession {
@@ -17,23 +17,25 @@ interface EditSession {
 })
 export class CountdownTimerComponent implements OnInit {
   @Input() editSession$;
+  @Input() editSession;
   @Output() counterRefresh: EventEmitter<any> = new EventEmitter();
   countDown$;
   counter: number;
   tick = 1000;
+  startTimer$ = new BehaviorSubject(false);
 
   @ViewChild('reset', {static: true})
   refreshBtn: ElementRef;
 
   ngOnInit(): void {
-    this.countDown$ = this.editSession$.pipe(
-      switchMap((session: EditSession) => {
-        this.counter = session.editTime;
+    this.countDown$ = this.startTimer$.pipe(
+      switchMap((session) => {
         return interval(this.tick)
           .pipe(
             mapTo(-1),
-            scan((acc, curr) => (curr ? curr + acc : acc), this.counter),
+            scan((acc, curr) => (curr ? curr + acc : acc), this.editSession),
             takeWhile(count => count >= 0),
+            tap(next => console.log(next)),
             finalize(() => this.counterRefresh.emit(false))
           );
       }),
